60db6d4362ac8f5c0e405ce5095a1384ee85ecaa
OzuAkira <22311038ao@tama.ac.jp>
2025-06-11 00:57:25 +0900

    データベースをどうしようか？

---
+++ Assets/script/battle_Flavour/F_numberSetting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F_numberSetting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int F_Num;
+    public void character_select(int C_index)
+    {
+        /*
+        ここに{list または array}の'F_Num'番目のデータを,
+        キャラクターデータベースの C_index 番目のデータに代入する
+        
+        by 2025/6/11 の俺
+        */
+    }
+}
--- Assets/script/battle_Flavour/Flavour_setting.cs
+++ Assets/script/battle_Flavour/Flavour_setting.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class first : menuScript
+public class Flavour_setting : menuScript
 {
     public GameObject characterList;
+    public int favour_number;
+
     //public GameObject cursor;
     void Awake()
     {
         characterList.SetActive(false);
     }
     public override void select()
-    {
-        //Debug.Log("activ_action");
+    { 
+        F_numberSetting f_NumberSetting = GetComponent<F_numberSetting>();
+        f_NumberSetting.F_Num = favour_number;
         characterList.SetActive(true);
     }
+
+
+
     void Update()
     {
-        if(characterList.activeSelf == true) gameObject.SetActive(false);
+        if (characterList.activeSelf == true) gameObject.SetActive(false);
     }
 }
