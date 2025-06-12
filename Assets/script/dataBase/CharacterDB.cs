bcce50d2b0b92ee98814ab083b827b2dcff5c781
OzuAkira <22311038ao@tama.ac.jp>
2025-06-12 11:57:07 +0900

    db途中

--- Assets/script/dataBase/CharacterDB.cs
+++ Assets/script/dataBase/CharacterDB.cs
@@ -8,18 +8,13 @@ public class CharacterDB : MonoBehaviour
     [CreateAssetMenu(menuName = "Character_table")]
     public class Character_table : ScriptableObject
     {
-        Dictionary<string, Dictionary<string, int>> CDB = new Dictionary<string, Dictionary<string, int>>
-        {
-            { "User1", new Dictionary<string,int>
-                {
-                    { "hp",0},{ "mp",0},{ "attack",0},{"defense",0},{"speed",0}
-                }
-            },
-            { "User2", new Dictionary<string,int>
-                {
-                    { "hp",0},{ "mp",0},{ "attack",0},{"defense",0},{"speed",0}
-                }
-            }
-        };
+        public int Character_id;
+        public int Character_name;
+        public int HP;
+        public int MP;
+        public int attack;
+        public int defense;
+        public int speed;
     }
 }
+
