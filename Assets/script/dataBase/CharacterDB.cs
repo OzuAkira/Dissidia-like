5dd3915bc74d69f84844af0d9ce1e199342cbab3
OzeAkira <22311038ao@tama.ac.jp>
2025-06-12 19:17:15 +0900

    データベースをなんか色々

--- Assets/script/dataBase/CharacterDB.cs
+++ Assets/script/dataBase/CharacterDB.cs
@@ -8,8 +8,13 @@ public class CharacterDB : MonoBehaviour
     [CreateAssetMenu(menuName = "Character_table")]
     public class Character_table : ScriptableObject
     {
+        public List<parameters> characterDB;
+    }
+    [System.Serializable]
+    public class parameters
+    {
         public int Character_id;
-        public int Character_name;
+        public string Character_name;
         public int HP;
         public int MP;
         public int attack;
