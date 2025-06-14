069bba25a3ff2caf04ea10b27786d13b1072ec31
OzuAkira <22311038ao@tama.ac.jp>
2025-06-14 15:15:51 +0900

    Update battle_start.cs

--- Assets/script/this_System/battle_start.cs
+++ Assets/script/this_System/battle_start.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static CharacterDB;
 
 public class battle_start : menuScript
 {
-    [SerializeField] GameObject gm;
+    [SerializeField] GameObject gm , backGround , battleMap;
+    [SerializeField] CharacterDB.Character_table character_table;
+    private F_numberSetting f_NumberSetting;
+    private List<parameters> battleMember;
+    
     public override void select()
     {
-        
+        f_NumberSetting = gm.GetComponent<F_numberSetting>();
+        foreach (parameters _DB in character_table._characterDB)
+        {
+            battleMember.Add(_DB);
+        }
+
+        battleMap.SetActive(true);
+
     }
 }
