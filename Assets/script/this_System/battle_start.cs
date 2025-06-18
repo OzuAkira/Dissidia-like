8b732240bdd78d3a696214c8a0d1bb2d2212f20f
OzuAkira <22311038ao@tama.ac.jp>
2025-06-18 12:14:23 +0900

    進捗

--- Assets/script/this_System/battle_start.cs
+++ Assets/script/this_System/battle_start.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static CharacterDB;
 
@@ -8,9 +9,10 @@ public class battle_start : menuScript
     [SerializeField] GameObject gm , setting , battleMap;
     [SerializeField] CharacterDB.Character_table character_table;
     [SerializeField] breakScene breakScene ;
+    public int _min = 1;
     private F_numberSetting f_NumberSetting;
     public parameters[] battleMember = { null,null,null};
-    
+
     public override void select()
     {
         /*
@@ -32,9 +34,29 @@ public class battle_start : menuScript
 
         }
         */
-        setting.SetActive(false);
-        battleMap.SetActive(true);
-        breakScene.StartCoroutine("BreakStart");
+        //bool start_flag = false;
+        int _count = 0;
+        f_NumberSetting = gm.GetComponent<F_numberSetting>();
+        foreach (int x in f_NumberSetting.num_id_cha)
+        {
+            if (x >= 0)
+            {
+                _count++;
+            }
+        }
+        if (_count == _min)
+        {
+            setting.SetActive(false);
+            battleMap.SetActive(true);
+            breakScene.StartCoroutine("BreakStart");
+        }
+        else
+        {
+            Debug.Log("キャラクターを選んでね");
+            //return;
+        }
+        
+
     }
     /*
     IEnumerator _battle()
