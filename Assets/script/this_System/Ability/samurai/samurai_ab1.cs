060133dba7ed3e8e5d25ba935e00b00807c22661
OzuAkira <22311038ao@tama.ac.jp>
2025-06-30 23:08:40 +0900

    abilityList作成途中

---
+++ Assets/script/this_System/Ability/samurai/samurai_ab1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class samurai_ab1 : abilities
+{
+    public override void Attack()
+    {
+        
+    }
+}
