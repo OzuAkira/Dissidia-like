f346ab94b8e7c40d8c5e316dc7744c94a951b8ce
OzuAkira <22311038ao@tama.ac.jp>
2025-07-19 21:09:10 +0900

    一旦、Notionで情報を整理してから作業をしようか

---
+++ Assets/script/status/buffs.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class buffs : MonoBehaviour
+{
+    abstract public void justadd();//•t—^‚³‚ê‚é‚Æ‚«
+
+}
